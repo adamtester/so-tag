4764cce908ed0a99ec0e8866f982b3043d5dcc15
Nick Wilde <design@briarmoon.ca>
2014-03-25 17:57:17 -0700

    Updated build method; can specify a 'build_dev' or a 'build_release' (default is both of those) or a 'build_dual' for grunt. build_release includes *only* minified js/css build_dev includes non-minified sources. build_dual includes both minifed and non-minified files

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,36 +4,75 @@ module.exports = function (grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        src_files_js: ['js/so_tag.js'],
+        src_files_css: ['css/*.css'],
+        src_files_other: ['*.json', '*.php', 'LICENSE', 'README.md', '!package.json'],
+        dest_dir_dev: 'build/dev/',
+        dest_dir_rel: 'build/release/',
+        dest_dir_dual: 'build/dual/',
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
-            build: {
-                src: 'js/so_tag.js',
-                dest: 'build/js/so_tag.min.js'
+            build_release: {
+                src: ['<%= src_files_js %>'],
+                dest: '<%= dest_dir_rel %>js/so_tag.min.js'
+            },
+            build_dual: {
+                src: ['<%= src_files_js %>'],
+                dest: '<%= dest_dir_dual %>js/so_tag.min.js'
             }
         },
         jshint: {
-            src: ['js/so_tag.js']
-        },
-        copy: {
             main: {
-                src: ['css/*', 'js/so_tag.js', '*.json', '*.php', 'LICENSE', 'README.md', '!package.json'],
-                dest: 'build/',
+                src: ['<%= src_files_js %>']
             },
         },
+        copy: {
+            build_release: {
+                src: ['<%= src_files_other %>'],
+                dest: '<%= dest_dir_rel %>',
+            },
+            build_dev: {
+                src: ['<%= src_files_js %>', '<%= src_files_css %>', '<%= src_files_other %>'],
+                dest: '<%= dest_dir_dev %>',
+            },
+            build_dual: {
+                src: ['<%= src_files_js %>', '<%= src_files_css %>', '<%= src_files_other %>'],
+                dest: '<%= dest_dir_dual %>',
+            }
+        },
         cssmin: {
-            minify: {
+            build_release: {
+                options : {
+                    report: 'gzip'
+                },
                 expand: true,
-                cwd: 'build/css/',
+                cwd: 'css/',
                 src: ['*.css', '!*.min.css'],
-                dest: 'build/css/',
+                dest: '<%= dest_dir_rel %>/css/',
+                ext: '.min.css'
+            },
+            build_dual: {
+                options : {
+                    report: 'gzip'
+                },
+                expand: true,
+                cwd: 'css/',
+                src: ['*.css', '!*.min.css'],
+                dest: '<%= dest_dir_dual %>/css/',
                 ext: '.min.css'
             }
         },
         clean: {
-            build: {
-                src: ["build/"]
+            build_release: {
+                src: ['<%= dest_dir_rel %>*']
+            },
+            build_dev: {
+                src: ['<%= dest_dir_dev %>*']
+            },
+            build_dual: {
+                src: ['<%= dest_dir_dual %>*']
             }
         }
     });
@@ -46,6 +85,9 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // Default task(s).
-    grunt.registerTask('default', ['clean', 'copy', 'jshint', 'uglify', 'cssmin']);
+    grunt.registerTask('build_dev', ['clean:build_dev', 'copy:build_dev', 'jshint']);
+    grunt.registerTask('build_release', ['clean:build_release', 'copy:build_release', 'jshint', 'uglify:build_release', 'cssmin:build_release']);
+    grunt.registerTask('build_dual', ['clean:build_dual', 'copy:build_dual', 'jshint', 'uglify:build_dual', 'cssmin:build_dual']);
+    grunt.registerTask('default', ['build_dev', 'build_release']);
 
 };
